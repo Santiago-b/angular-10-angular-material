6bc9ae5cb7ab78915be83d2bd78cc940b6ed0539
Santiago-b <santi09411@hotmail.com>
2023-07-05 18:31:57 -0500

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent, ExampleHeader } from './app.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -18,6 +18,7 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatGridListModule,
     CommonModule,
     MatNativeDateModule,
+    ReactiveFormsModule,
   ],
   declarations: [AppComponent, ExampleHeader],
   bootstrap: [AppComponent],
